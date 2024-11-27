40e3688b5c129e0c5e337e9a7f9f5e029c0dbf40
sjuma <sheillajuma001@gmail.com>
2024-11-27 14:52:19 +0300

    testing hello world

---
+++ hello_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	got := Hello()
+	want := "Hello, World"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	} 
+}
